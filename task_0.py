d5f9ca470f02b762c3745ab2c35b694a82062ee5
bijaybishwakarma <bijaybcofficial13@gmail.com>
2026-01-02 07:47:31 +0545

    My first Python program using strings and functions

---
+++ task_0.py
@@ -0,0 +1,10 @@
+#Task:0 WASP(taking the use name and fav_word print them in to uppercase and lowercase also return length of them)
+user_name = input("Enter your name here: ")
+
+print(f"Hello, Mr.{user_name.lower()}")
+
+fav_word = input("Enter your favorite word: ")
+print(f"Your favorite word in uppercase: {fav_word.upper()}")
+print(f"Length of this word is: {len(fav_word)}")
+
+
